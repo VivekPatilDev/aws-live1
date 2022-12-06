5c3711aa70f65e3e5d50cc355adb1dc764f0efb8
VivekPatilDev <119923454+VivekPatilDev@users.noreply.github.com>
2022-12-06 15:37:22 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee9921.caisjxmxnqnr.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "addemployee9921"
+customregion = "ap-south-1"
